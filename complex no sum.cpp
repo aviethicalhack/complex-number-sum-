05e072f0d747f1631d2f046b11651a2c26c2741e
AVIRAL MISHRA <61314059+aviethicalhack@users.noreply.github.com>
2020-03-22 11:30:28 +0530

    Add files via upload

---
+++ complex no sum.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+  int i,j,k,l,m,n;
+  cout<<"enter two imaginary number\n";
+  cin>>i>>j;
+ cout<<"enter the  two real number\n";
+  cin>>k>>l;
+  m=i+j;
+  n=k+l;
+  cout<<n<<"+"<<m<<"i";
+}
